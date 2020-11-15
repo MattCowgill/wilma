2ecef47114ad4754ca03c46d42d15d3f86df6c03
Matt Cowgill <mattcowgill@gmail.com>
2020-11-15 14:46:30 +1100

    add date slider

--- R/wilma_app.R
+++ R/wilma_app.R
@@ -6,6 +6,9 @@ wilma <- function(...) {
         uiOutput("series_select")
         ),
     fluidRow(
+      uiOutput("date_select")
+    ),
+    fluidRow(
       column(
         width = 12,
         ggiraph::girafeOutput("plot",
@@ -25,15 +28,34 @@ wilma <- function(...) {
                   width = '100%')
     })
 
-    chart_data <- reactive({
+    selected_id <- reactive({
       shiny::req(input$series_select)
       selected_series <- input$series_select
-      selected_id <- as.character(available_data$series_id[available_data$series == selected_series])
-      chart_data <- data[[selected_id]]
+      as.character(available_data$series_id[available_data$series == selected_series])
+    })
+
+    chart_data <- reactive({
+      chart_data <- data[[selected_id()]]
+    })
+
+    output$date_select <- renderUI({
+      req(chart_data())
+      min_data_date <- min(chart_data()$date)
+      max_data_date <- max(chart_data()$date)
+      sliderInput("date_select",
+                  label = "Select date range",
+                  min = min_data_date,
+                  max = max_data_date,
+                  value = c(min_data_date, max_data_date))
     })
 
     output$plot <- ggiraph::renderGirafe({
-      static_plot <- make_graph(chart_data())
+      req(chart_data())
+      req(input$date_select)
+      static_plot <- make_graph(chart_data = chart_data(),
+                                start_date = input$date_select[1],
+                                end_date = input$date_select[2])
+
       ggiraph::girafe(ggobj = static_plot, width_svg = 9, height_svg = 5)
     })
 
