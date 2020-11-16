dac685c31ea1931a992f02b88e1d3ed335f9a5b9
Matt Cowgill <mattcowgill@gmail.com>
2020-11-16 15:44:49 +1100

    fix test

--- tests/testthat/test-make_graph.R
+++ tests/testthat/test-make_graph.R
@@ -1,5 +1,5 @@
 test_that("make_graph() makes a graph", {
-  chart_data <- data$A2599049K
+  chart_data <- dplyr::filter(data, .data$series_id == "A84423050A")
   plot <- make_graph(chart_data)
 
   expect_is(plot, "ggplot")
